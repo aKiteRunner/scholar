dc6bcb79524b5104bcf2d661b64decd5a0a8cd3c
marco <1007284166@qq.com>
2018-05-31 21:38:24 +0800

    test

--- src/main/java/com/web/service/TopUpService.java
+++ src/main/java/com/web/service/TopUpService.java
@@ -1,16 +1,13 @@
 package com.web.service;
 
-import com.web.bean.CreditOrder;
-import com.web.dao.CreditOrderMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+//import com.web.dao.CreditOrderMapper;
 
 // 充值
-@Service
-public class TopUpService {
-    @Autowired
-    private CreditOrderMapper creditOrderMapper;
-
-    public void insertOrder() {
-    }
-}
+//@Service
+//public class TopUpService {
+//    @Autowired
+//    private CreditOrderMapper creditOrderMapper;
+//
+//    public void insertOrder() {
+//    }
+//}
