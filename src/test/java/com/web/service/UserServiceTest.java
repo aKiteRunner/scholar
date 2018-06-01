438ed67f3e64589dd7b8b1664814c1622c5669e3
aKiteRunner <mengyudongchina@126.com>
2018-06-01 15:02:16 +0800

    新增UserServiceTest进行测试

---
+++ src/test/java/com/web/service/UserServiceTest.java
@@ -0,0 +1,23 @@
+package com.web.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:applicationContext.xml", "classpath:MyBatis-config.xml"})
+public class UserServiceTest {
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void userExist() {
+        assertFalse(userService.userExist(-1));
+        assertTrue(userService.userExist(1));
+    }
+}
